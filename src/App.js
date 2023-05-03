17e0827eae8f9233687bc19c6ae0205bcbabf4b1
nianokia <111546789+nianokia@users.noreply.github.com>
2023-05-03 18:24:28 -0400

    Initial Commit with Bootstrap

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello World</h1>
+      </header>
+    </div>
+  );
+}
