759f2ba44649d7076f11026165073fb72e4ceb67
nianokia <111546789+nianokia@users.noreply.github.com>
2023-05-03 19:49:22 -0400

    Added main components and css files
    Added Dictionary component along with its corresponding css file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Hello World</h1>
-      </header>
+      <div className="container">
+        <header className="App-header text-center">
+          <h1>Dictionary App</h1>
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer>
+          This app was coded by{" "}
+          <a
+            href="https://niawright.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Nia Wright
+          </a>{" "}
+          and is open-sourced on{" "}
+          <a
+            href="https://github.com/nianokia/dictionary-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Github
+          </a>
+          .
+        </footer>
+      </div>
     </div>
   );
 }
