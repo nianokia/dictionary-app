c923cc36df2dd901b6565c0d51f5e34ecd21fde1
nianokia <111546789+nianokia@users.noreply.github.com>
2023-05-03 20:06:37 -0400

    Added Search Engine

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./Dictionary.css";
 
 export default function Dictionary() {
-  return <div className="Dictionary">Hello from Dictionary</div>;
+  let [query, setQuery] = useState(null);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    alert(`Searching for ${query}`);
+  }
+
+  function updateQuery(event) {
+    setQuery(event.target.value);
+  }
+
+  return (
+    <div className="Dictionary">
+      <form onSubmit={handleSubmit}>
+        <input
+          type="search"
+          placeholder="Search for a word..."
+          onChange={updateQuery}
+        />
+      </form>
+    </div>
+  );
 }
